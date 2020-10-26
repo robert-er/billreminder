e4eecfa3ccf1b53152a1df34c9ccaca8c8d1fdd6
robert-er <robert.roza.pl@gmail.com>
2020-10-26 06:36:01 +0100

    initial commit

---
+++ src/main/java/com/billreminder/BillreminderApplication.java
@@ -0,0 +1,13 @@
+package com.billreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillreminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillreminderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/billreminder/BillreminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillreminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
