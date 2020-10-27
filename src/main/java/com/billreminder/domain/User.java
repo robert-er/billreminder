71d7a5c33de6343300a10f64f6a2051066b50eef
robert-er <robert.roza.pl@gmail.com>
2020-10-27 10:16:47 +0100

    User

--- src/main/java/com/billreminder/domain/User.java
+++ src/main/java/com/billreminder/domain/User.java
@@ -1,13 +1,14 @@
 package com.billreminder.domain;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
